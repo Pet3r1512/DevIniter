ff74bef77f6052fa78313299e15e7b8f6bac160d
Thanh Phong <pttp15122002@gmail.com>
2024-10-07 18:33:24 +0700

    Style: update tailwind config for selector dark/light mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 // import colors from "tailwindcss/colors";
 
 const config: Config = {
@@ -7,7 +8,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class", // Enabling dark mode with 'class' strategy
+  darkMode: ["selector"],
   theme: {
     extend: {
       colors: {
@@ -27,9 +28,14 @@ const config: Config = {
           dark: "#212529",
         },
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
-};
+  plugins: [animate],
+} satisfies Config;
 
 export default config;
