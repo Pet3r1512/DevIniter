06f79e1425576bb720e8363871da945291f375f6
Thanh Phong <pttp15122002@gmail.com>
2024-10-07 12:20:47 +0700

    Feat: Logo component

---
+++ src/components/Layouts/Header/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <Image
+      src={"/images/logos/FullLogo.png"}
+      alt=""
+      width={0}
+      height={0}
+      sizes="100vw"
+      className="lg:h-12 w-auto"
+    />
+  );
+}
