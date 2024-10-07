9705bca5243bf4cbd9ce55bccbd7070ddeb80917
Thanh Phong <pttp15122002@gmail.com>
2024-10-07 16:05:09 +0700

    Feat: add search bar to header

--- src/components/Layouts/Header/_index.tsx
+++ src/components/Layouts/Header/_index.tsx
@@ -1,11 +1,15 @@
 import ThemeModeSwitcher from "../ThemeToggle";
 import Logo from "./Logo";
+import SearchBar from "./SearchBar/_index";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between py-5">
+    <header className="flex items-center justify-between py-5 px-2.5 lg:px-0">
       <Logo />
-      <ThemeModeSwitcher />
+      <div className="flex items-center gap-x-5">
+        <SearchBar />
+        <ThemeModeSwitcher />
+      </div>
     </header>
   );
 }
