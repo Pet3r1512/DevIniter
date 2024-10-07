32cb5e4b4ab55f60dc12e35a869a6c8299e8a833
Thanh Phong <pttp15122002@gmail.com>
2024-10-07 20:58:22 +0700

    Fix: missing logo on desktops

--- src/components/Layouts/Header/Logo.tsx
+++ src/components/Layouts/Header/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
       width={0}
       height={0}
       sizes="100vw"
-      className="lg:h-12 w-auto h-10 max-w-1/3 lg:max-w-0"
+      className="lg:h-12 w-auto h-10 max-w-1/3 lg:max-w-none"
     />
   );
 }
