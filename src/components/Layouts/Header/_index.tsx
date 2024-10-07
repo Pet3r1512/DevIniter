ba48f2d77d46190ec4877c1175653e2885361479
Thanh Phong <pttp15122002@gmail.com>
2024-10-07 18:35:53 +0700

    Feat(cont): header section

--- src/components/Layouts/Header/_index.tsx
+++ src/components/Layouts/Header/_index.tsx
@@ -1,15 +1,31 @@
-import ThemeModeSwitcher from "../ThemeToggle";
+import { Menu } from "lucide-react";
+import Github from "./Github";
 import Logo from "./Logo";
+import Navigation from "./Navigations";
 import SearchBar from "./SearchBar/_index";
+import ThemeToggle from "../ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between py-5 px-2.5 lg:px-0">
+    <header className="flex items-center justify-between py-5 px-5 lg:px-0">
       <Logo />
-      <div className="flex items-center gap-x-5">
+      <Navigation />
+      <div className="flex items-center gap-x-5 lg:hidden">
         <SearchBar />
-        <ThemeModeSwitcher />
+        <Menu className="lg:hidden" size={20} />
+      </div>
+      <div className="lg:flex items-center gap-x-5 hidden">
+        <SearchBar />
+        <ThemeToggle />
+        <VerticalLine />
+        <Github size={20} />
       </div>
     </header>
+  );
+}
+
+function VerticalLine() {
+  return (
+    <div className="border-l-2 border-black-main dark:border-white h-[25px]"></div>
   );
 }
