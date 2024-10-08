17e7fafa6b3dd4e76fd68d80446bfdc0d05fddea
Thanh Phong <pttp15122002@gmail.com>
2024-10-08 12:12:13 +0700

    Chore: update page name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,11 @@ import Template from "@/components/Layouts/Template";
 
 export default function Home() {
   return (
-    <Template className="w-full lg:w-screen px-0" sectionClassName="max-w-0">
+    <Template
+      pageName="Developer Initializer"
+      className="w-full lg:w-screen px-0"
+      sectionClassName="max-w-0"
+    >
       <Hero />
       <p className="h-[1000px]"></p>
     </Template>
