61caa4f3a9ed952cbac169a0c0812c5242fb0eb7
Thanh Phong <pttp15122002@gmail.com>
2024-10-08 10:59:42 +0700

    Refactor: navbar position

--- src/components/Layouts/Header/_index.tsx
+++ src/components/Layouts/Header/_index.tsx
@@ -7,18 +7,22 @@ import Sidebar from "./Sidebar/_index";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between py-5 px-5 lg:px-0 sticky top-0 z-10 !backdrop-filter !backdrop-blur-sm">
-      <Sidebar />
-      <Logo />
-      <Navigation />
-      <div className="flex items-center gap-x-5 lg:hidden w-1/3 justify-end">
-        <SearchBar />
-      </div>
-      <div className="lg:flex items-center gap-x-5 hidden">
-        <SearchBar />
-        <ThemeToggle />
-        <VerticalLine />
-        <Github size={20} />
+    <header className="flex items-center justify-between py-5 px-5 lg:px-0 sticky top-0 z-20 !backdrop-filter !backdrop-blur-xl">
+      <div className="max-w-[1440px] w-full mx-auto flex items-center justify-between">
+        <Sidebar />
+        <div className="flex items-center gap-x-8">
+          <Logo />
+          <Navigation />
+        </div>
+        <div className="flex items-center gap-x-5 lg:hidden w-1/3 justify-end">
+          <SearchBar />
+        </div>
+        <div className="lg:flex items-center gap-x-5 hidden">
+          <SearchBar />
+          <ThemeToggle />
+          <VerticalLine />
+          <Github size={20} />
+        </div>
       </div>
     </header>
   );
