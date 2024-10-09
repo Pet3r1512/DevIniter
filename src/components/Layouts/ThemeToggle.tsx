37ce640d3a33b92d06ab353de18170edc50b314b
Thanh Phong <pttp15122002@gmail.com>
2024-10-09 20:45:56 +0700

    Refactor: add custom className

--- src/components/Layouts/ThemeToggle.tsx
+++ src/components/Layouts/ThemeToggle.tsx
@@ -1,15 +1,19 @@
 import { useTheme } from "@/hooks/useTheme";
+import { cn } from "@/utils/utils";
 
-const ThemeToggle = () => {
+const ThemeToggle = ({ className }: { className?: string }) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <button
       data-testid="theme-togger"
       onClick={toggleTheme}
-      className="h-8 lg:h-10 w-auto rounded-full lg:text-lg bg-gray-500 dark:bg-gray-200 transition-all dark:text-black text-white px-2.5"
+      className={cn(
+        "h-8 lg:h-10 px-5 py-2 w-auto rounded-full lg:text-lg bg-gray-500 dark:bg-gray-200 transition-all dark:text-black text-white duration-150 ease-linear flex items-center justify-center ",
+        className
+      )}
     >
-      {theme === "light" ? "🌙 Dark" : "☀️ Light"}
+      {theme === "light" ? "🌙 Dark" : "Light ☀️"}
     </button>
   );
 };
