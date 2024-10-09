998b742d9613d1dac973f0dfba73bbbb94699f66
Thanh Phong <pttp15122002@gmail.com>
2024-10-09 21:29:24 +0700

    Feat: sidebar content

--- src/components/Layouts/Header/Sidebar/_index.tsx
+++ src/components/Layouts/Header/Sidebar/_index.tsx
@@ -1,5 +1,10 @@
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { Menu } from "lucide-react";
+import { navs } from "../Navigations";
+import Link from "next/link";
+import Logo from "../Logo";
+import ThemeToggle from "../../ThemeToggle";
+import Github from "../Github";
 
 export default function Sidebar() {
   return (
@@ -7,7 +12,20 @@ export default function Sidebar() {
       <DrawerTrigger id="sidebar-trigger" className="lg:hidden w-1/3">
         <Menu size={24} />
       </DrawerTrigger>
-      <DrawerContent className="lg:hidden h-[100dvh] w-2/3"></DrawerContent>
+      <DrawerContent className="lg:hidden h-[100dvh] w-2/3 flex flex-col p-5 gap-y-10">
+        <Logo className="h-auto w-2/3" />
+        <aside className="flex flex-col gap-y-2.5 px-5">
+          {navs.map((nav, index) => {
+            return (
+              <Link key={index} href={nav.href} className="font-semibold">
+                {nav.name}
+              </Link>
+            );
+          })}
+        </aside>
+        <ThemeToggle className="w-fit h-auto mt-24" />
+        <Github size={24} className="w-fit" />
+      </DrawerContent>
     </Drawer>
   );
 }
