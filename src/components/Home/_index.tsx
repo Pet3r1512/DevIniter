4fe8f3ee7685fbb41f7ab46071bdee76e2b380fe
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-10-09 17:23:44 +0700

    #FEAT: Nextjs Template Highlight Section (#7)
    * Style: add white light color
    
    * Refactor: remove unused html tag
    
    * Feat: hightlight section
    
    * Feat: Bento grid section
    
    * Feat: Highlight's title section
    
    * Feat: svgs
    
    * Feat: component from acternity
    
    * Feat: add new section to home page
    
    * Style: update white color
    
    * Style: responsive
    
    * Fix: bug when svg's width is too long on mobiles
    
    * Style: add margin bottom on mobiles
    
    * Fix: unsupport eslint version with typescript

--- src/components/Home/_index.tsx
+++ src/components/Home/_index.tsx
@@ -3,9 +3,9 @@ import Process from "./Hero/Process";
 
 export function Hero() {
   return (
-    <section>
+    <>
       <Introduction />
       <Process />
-    </section>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import { Hero } from "@/components/Home/_index";
+import Highlight from "@/components/Home/Highlight/_index";
 import Template from "@/components/Layouts/Template";
 
 export default function Home() {
   return (
     <Template
       pageName="Developer Initializer"
-      className="w-full lg:w-screen px-0"
+      className="w-full max-w-none lg:w-screen px-0"
       sectionClassName="max-w-0"
     >
       <Hero />
-      <p className="h-[1000px]"></p>
+      <section className="w-screen lg:max-w-7xl min-h-screen mx-auto flex flex-col items-center mt-[50vh] sm:mt-[15vh] lg:mt-[35vh] lg:mb-24 mb-16">
+        <Highlight />
+      </section>
     </Template>
   );
 }
