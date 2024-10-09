5faf3fcc76bf520e06d33c6f47ce272c119dc71b
Thanh Phong <pttp15122002@gmail.com>
2024-10-09 16:10:27 +0700

    Feat: svgs

---
+++ src/components/Home/Highlight/images/Lightning.tsx
@@ -0,0 +1,30 @@
+const Lightning = () => (
+  <div className="lg:hover:text-gold lg:text-gray text-gold transition-colors duration-300">
+    <svg
+      fill="currentColor"
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 560.317 560.316"
+      xmlSpace="preserve"
+      className="lg:size-48 size-28"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <g>
+            <path d="M207.523,560.316c0,0,194.42-421.925,194.444-421.986l10.79-23.997c-41.824,12.02-135.271,34.902-135.57,35.833 C286.96,122.816,329.017,0,330.829,0c-39.976,0-79.952,0-119.927,0l-12.167,57.938l-51.176,209.995l135.191-36.806 L207.523,560.316z"></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  </div>
+);
+
+export default Lightning;
---
+++ src/components/Home/Highlight/images/TnT.tsx
@@ -0,0 +1,229 @@
+export default function TnT() {
+  return (
+    <section className="flex flex-wrap gap-3.5 lg:gap-7 justify-center">
+      <div className="hidden lg:size-[85px] size-[75px] bg-white dark:bg-black rounded-lg blur-[2.5px] p-4 lg:flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105"></div>
+      <div className="hidden lg:size-[85px] size-[75px] bg-white dark:bg-black rounded-lg blur-[2.25px] p-4 lg:flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105"></div>
+      <div className="lg:size-[85px] size-[75px] bg-white dark:bg-black lg:hover:bg-gradient-to-br from-gray-light dark:to-black to-gray rounded-lg p-4 flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105">
+        <svg
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          className="size-[50px]"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <g clipPath="url(#clip0)">
+              <path d="M11.2141 0.00645944C11.1625 0.0111515 10.9982 0.0275738 10.8504 0.039304C7.44164 0.346635 4.24868 2.18593 2.22639 5.01291C1.10029 6.58476 0.380059 8.36775 0.107918 10.2563C0.0117302 10.9156 0 11.1103 0 12.0041C0 12.898 0.0117302 13.0927 0.107918 13.7519C0.760117 18.2587 3.96716 22.0452 8.31672 23.4481C9.0956 23.6991 9.91672 23.8704 10.8504 23.9736C11.2141 24.0135 12.7859 24.0135 13.1496 23.9736C14.7613 23.7953 16.1267 23.3965 17.4733 22.7091C17.6798 22.6035 17.7196 22.5754 17.6915 22.5519C17.6727 22.5378 16.793 21.3578 15.7372 19.9314L13.8182 17.339L11.4135 13.7801C10.0903 11.8235 9.00176 10.2235 8.99238 10.2235C8.98299 10.2211 8.97361 11.8024 8.96891 13.7331C8.96188 17.1138 8.95953 17.2499 8.9173 17.3296C8.85631 17.4446 8.80938 17.4915 8.71085 17.5431C8.63578 17.5807 8.57009 17.5877 8.21584 17.5877H7.80997L7.70205 17.5197C7.63167 17.4751 7.58006 17.4164 7.54487 17.3484L7.4956 17.2428L7.50029 12.539L7.50733 7.83285L7.58006 7.74136C7.6176 7.69209 7.69736 7.62875 7.75367 7.59825C7.84985 7.55133 7.88739 7.54664 8.29325 7.54664C8.77185 7.54664 8.85161 7.5654 8.97595 7.70147C9.01114 7.73901 10.3132 9.7003 11.871 12.0628C13.4287 14.4252 15.5589 17.651 16.6053 19.2346L18.5056 22.1132L18.6018 22.0499C19.4534 21.4962 20.3543 20.7079 21.0674 19.8868C22.5853 18.1437 23.5636 16.0182 23.8921 13.7519C23.9883 13.0927 24 12.898 24 12.0041C24 11.1103 23.9883 10.9156 23.8921 10.2563C23.2399 5.74957 20.0328 1.96306 15.6833 0.560125C14.9161 0.311445 14.0997 0.140184 13.1848 0.036958C12.9595 0.0134976 11.4088 -0.0123089 11.2141 0.00645944ZM16.1267 7.26511C16.2393 7.32142 16.3308 7.42933 16.3636 7.54194C16.3824 7.60294 16.3871 8.90734 16.3824 11.8469L16.3754 16.0651L15.6317 14.9249L14.8856 13.7848V10.7185C14.8856 8.73608 14.895 7.62171 14.9091 7.56775C14.9466 7.43637 15.0287 7.33315 15.1413 7.27215C15.2375 7.22288 15.2727 7.21819 15.6411 7.21819C15.9883 7.21819 16.0493 7.22288 16.1267 7.26511Z"></path>
+            </g>
+            <defs>
+              <clipPath id="clip0">
+                <rect width="24" height="24" fill="white"></rect>
+              </clipPath>
+            </defs>
+          </g>
+        </svg>
+      </div>
+      <div className="lg:size-[85px] size-[75px] bg-white dark:bg-black lg:hover:bg-gradient-to-br from-white to-[#44a8b3]/50 rounded-lg p-4 flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          className="h-[50px] w-auto max-w-[50px]"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <title>file_type_tailwind</title>
+            <path
+              d="M9,13.7q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q11.1,10.9,9,13.7ZM2,22.1q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q4.1,19.3,2,22.1Z"
+              fill="#44a8b3"
+            />
+          </g>
+        </svg>
+      </div>
+      <div className="hidden lg:size-[85px] size-[75px] bg-white dark:bg-black rounded-lg blur-[2.25px] p-4 lg:flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105"></div>
+      <div className="hidden lg:size-[85px] size-[75px] bg-white dark:bg-black rounded-lg blur-[2.5px] p-4 lg:flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105"></div>
+      <div className="hidden lg:size-[85px] size-[75px] bg-white dark:bg-black rounded-lg blur-[2.5px] p-4 lg:flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105"></div>
+      <div className="lg:size-[85px] size-[75px] bg-white dark:bg-black lg:hover:bg-gradient-to-br from-white to-[#3178c6]/65 rounded-lg p-4 flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          className="size-[50px]"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <title>file_type_typescript_official</title>
+            <rect
+              x="2"
+              y="2"
+              width="28"
+              height="28"
+              rx="1.312"
+              fill="#3178c6"
+            ></rect>
+            <path
+              d="M18.245,23.759v3.068a6.492,6.492,0,0,0,1.764.575,11.56,11.56,0,0,0,2.146.192,9.968,9.968,0,0,0,2.088-.211,5.11,5.11,0,0,0,1.735-.7,3.542,3.542,0,0,0,1.181-1.266,4.469,4.469,0,0,0,.186-3.394,3.409,3.409,0,0,0-.717-1.117,5.236,5.236,0,0,0-1.123-.877,12.027,12.027,0,0,0-1.477-.734q-.6-.249-1.08-.484a5.5,5.5,0,0,1-.813-.479,2.089,2.089,0,0,1-.516-.518,1.091,1.091,0,0,1-.181-.618,1.039,1.039,0,0,1,.162-.571,1.4,1.4,0,0,1,.459-.436,2.439,2.439,0,0,1,.726-.283,4.211,4.211,0,0,1,.956-.1,5.942,5.942,0,0,1,.808.058,6.292,6.292,0,0,1,.856.177,5.994,5.994,0,0,1,.836.3,4.657,4.657,0,0,1,.751.422V13.9a7.509,7.509,0,0,0-1.525-.4,12.426,12.426,0,0,0-1.9-.129,8.767,8.767,0,0,0-2.064.235,5.239,5.239,0,0,0-1.716.733,3.655,3.655,0,0,0-1.171,1.271,3.731,3.731,0,0,0-.431,1.845,3.588,3.588,0,0,0,.789,2.34,6,6,0,0,0,2.395,1.639q.63.26,1.175.509a6.458,6.458,0,0,1,.942.517,2.463,2.463,0,0,1,.626.585,1.2,1.2,0,0,1,.23.719,1.1,1.1,0,0,1-.144.552,1.269,1.269,0,0,1-.435.441,2.381,2.381,0,0,1-.726.292,4.377,4.377,0,0,1-1.018.105,5.773,5.773,0,0,1-1.969-.35A5.874,5.874,0,0,1,18.245,23.759Zm-5.154-7.638h4V13.594H5.938v2.527H9.92V27.375h3.171Z"
+              fill="#ffffff"
+              fillRule="evenodd"
+            />
+          </g>
+        </svg>
+      </div>
+      <div className="lg:size-[85px] size-[75px] bg-white dark:bg-black lg:hover:bg-gradient-to-br from-white to-[#4b32c3]/75 rounded-lg p-4 flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          className="size-[50px]"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <title>file_type_eslint2</title>
+            <path
+              d="M13.027,30,2.39,20.422l2.979-14L18.973,2,29.61,11.578l-2.979,14L13.027,30ZM7.883,18.642l6.342,5.7L22.337,21.7l1.781-8.344L17.775,7.644,9.663,10.3,7.883,18.642Z"
+              fill="#4b32c3"
+            ></path>
+            <path
+              d="M21.72,25.915H10.28L4.554,16,10.28,6.085H21.72L27.446,16,21.72,25.915Zm-9.426-3.491h7.413L23.419,16,19.707,9.576H12.293L8.593,16l3.7,6.424Z"
+              fill="#8080f2"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="lg:size-[85px] size-[75px] bg-white dark:bg-black lg:hover:bg-gradient-to-br from-white to-[#ea5e5e]/85 rounded-lg p-4 flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105">
+        <svg
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#000000"
+          className="w-[50px] h-auto max-h-[50px]"
+        >
+          <path
+            d="M21.714,8.571h1.143a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H21.714a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,21.714,8.571Z"
+            style={{ fill: "#56b3b4" }}
+          />
+          <path
+            d="M4.571,26.857h5.714a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H4.571A.571.571,0,0,1,4,27.429H4A.571.571,0,0,1,4.571,26.857Z"
+            style={{ fill: "#ea5e5e" }}
+          />
+          <path
+            d="M18.286,17.714h3.429a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H18.286a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,18.286,17.714Z"
+            style={{ fill: "#bf85bf" }}
+          />
+          <path
+            d="M11.429,17.714H16a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H11.429a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,11.429,17.714Z"
+            style={{ fill: "#ea5e5e" }}
+          />
+          <path
+            d="M4.571,17.714H9.143a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H4.571A.571.571,0,0,1,4,18.286H4A.571.571,0,0,1,4.571,17.714Z"
+            style={{ fill: "#56b3b4" }}
+          />
+          <path
+            d="M4.571,22.286h5.714a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H4.571A.571.571,0,0,1,4,22.857H4A.571.571,0,0,1,4.571,22.286Z"
+            style={{ fill: "#bf85bf" }}
+          />
+          <path
+            d="M4.571,13.143h5.714a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H4.571A.571.571,0,0,1,4,13.714H4A.571.571,0,0,1,4.571,13.143Z"
+            style={{ fill: "#bf85bf" }}
+          />
+          <path
+            d="M10.286,6.286H21.714a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H10.286a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,10.286,6.286Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M4.571,6.286H8a.571.571,0,0,1,.571.571h0A.571.571,0,0,1,8,7.429H4.571A.571.571,0,0,1,4,6.857H4A.571.571,0,0,1,4.571,6.286Z"
+            style={{ fill: "#ea5e5e" }}
+          />
+          <path
+            d="M9.143,24.571h1.143a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H9.143a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,9.143,24.571Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M9.143,10.857h1.143a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H9.143a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,9.143,10.857Z"
+            style={{ fill: "#56b3b4" }}
+          />
+          <path
+            d="M4.571,24.571H6.857a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H4.571A.571.571,0,0,1,4,25.143H4A.571.571,0,0,1,4.571,24.571Z"
+            style={{ fill: "#56b3b4" }}
+          />
+          <path
+            d="M4.571,10.857H6.857a.571.571,0,0,1,.571.571h0A.571.571,0,0,1,6.857,12H4.571A.571.571,0,0,1,4,11.429H4A.571.571,0,0,1,4.571,10.857Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M19.429,24.571h1.143a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H19.429a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,19.429,24.571Z"
+            style={{ fill: "#4d616e", opacity: 0.5 }}
+          />
+          <path
+            d="M12.571,24.571h4.571a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H12.571A.571.571,0,0,1,12,25.143h0A.571.571,0,0,1,12.571,24.571Z"
+            style={{ fill: "#4d616e", opacity: 0.5 }}
+          />
+          <path
+            d="M22.857,24.571h4.571a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H22.857a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,22.857,24.571Z"
+            style={{ fill: "#4d616e", opacity: 0.5 }}
+          />
+          <path
+            d="M13.714,15.429h9.143a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H13.714A.571.571,0,0,1,13.143,16h0A.571.571,0,0,1,13.714,15.429Z"
+            style={{ fill: "#56b3b4" }}
+          />
+          <path
+            d="M8,15.429h3.429A.571.571,0,0,1,12,16h0a.571.571,0,0,1-.571.571H8A.571.571,0,0,1,7.429,16h0A.571.571,0,0,1,8,15.429Z"
+            style={{ fill: "#56b3b4" }}
+          />
+          <path
+            d="M5.714,21.429H8.571a.571.571,0,0,1,.571.571h0A.571.571,0,0,1,8.571,22H5.714A.571.571,0,0,1,5.143,21.857h0A.571.571,0,0,1,5.714,21.429Z"
+            style={{ fill: "#4d616e", opacity: 0.5 }}
+          />
+          <path
+            d="M8.571,5.714H14.857a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H8.571A.571.571,0,0,1,8,6.286h0A.571.571,0,0,1,8.571,5.714Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M5.714,11.429H7.429a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H5.714A.571.571,0,0,1,5.143,12h0A.571.571,0,0,1,5.714,11.429Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M5.714,16.571H7.429a.571.571,0,0,1,.571.571h0a.571.571,0,0,1-.571.571H5.714A.571.571,0,0,1,5.143,17h0A.571.571,0,0,1,5.714,16.571Z"
+            style={{ fill: "#4d616e", opacity: 0.5 }}
+          />
+          <path
+            d="M5.714,22.857H7.429a.571.571,0,0,1,.571.571h0A.571.571,0,0,1,7.429,23.428H5.714A.571.571,0,0,1,5.143,23h0A.571.571,0,0,1,5.714,22.857Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M22.857,10.857h4.571a.571.571,0,0,1,.571.571h0A.571.571,0,0,1,27.428,12H22.857A.571.571,0,0,1,22.286,11.429h0A.571.571,0,0,1,22.857,10.857Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M5.714,10.857H7.429a.571.571,0,0,1,.571.571h0A.571.571,0,0,1,7.429,12H5.714A.571.571,0,0,1,5.143,11.429h0A.571.571,0,0,1,5.714,10.857Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+          <path
+            d="M18.286,17.714h4.571a.571.571,0,0,1,.571.571h0A.571.571,0,0,1,22.857,18H18.286a.571.571,0,0,1-.571-.571h0A.571.571,0,0,1,18.286,17.714Z"
+            style={{ fill: "#f7ba3e" }}
+          />
+        </svg>
+      </div>
+      <div className="hidden lg:size-[85px] size-[75px] bg-white dark:bg-black rounded-lg blur-[2.5px] p-4 lg:flex items-center justify-center shadow-2xl transition-transform duration-300 hover:scale-105"></div>
+    </section>
+  );
+}
