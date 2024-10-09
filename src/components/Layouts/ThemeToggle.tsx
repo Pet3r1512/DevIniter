849e9f31ff2e17d63d83139a8d70a82c61046223
Thanh Phong <pttp15122002@gmail.com>
2024-10-09 21:29:42 +0700

    Chore: remove blank

--- src/components/Layouts/ThemeToggle.tsx
+++ src/components/Layouts/ThemeToggle.tsx
@@ -9,7 +9,7 @@ const ThemeToggle = ({ className }: { className?: string }) => {
       data-testid="theme-togger"
       onClick={toggleTheme}
       className={cn(
-        "h-8 lg:h-10 px-5 py-2 w-auto rounded-full lg:text-lg bg-gray-500 dark:bg-gray-200 transition-all dark:text-black text-white duration-150 ease-linear flex items-center justify-center ",
+        "h-8 lg:h-10 px-5 py-2 w-auto rounded-full lg:text-lg bg-gray-500 dark:bg-gray-200 transition-all dark:text-black text-white duration-150 ease-linear flex items-center justify-center",
         className
       )}
     >
