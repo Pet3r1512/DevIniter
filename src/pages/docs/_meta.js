3703a68c8c21e28a25b0e177a09f1e151a3594da
Thanh Phong <pttp15122002@gmail.com>
2024-10-10 15:22:33 +0700

    Chore: update section's name

--- src/pages/docs/_meta.js
+++ src/pages/docs/_meta.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   introduction: "Introduction",
-  get_started: "Get Started",
+  get_started: "Getting Started",
   installation: "Installation",
 };
