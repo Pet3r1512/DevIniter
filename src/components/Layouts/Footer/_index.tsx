c4ed70613633f7bd4e880214d45e6669c9f33670
Thanh Phong <pttp15122002@gmail.com>
2024-10-10 10:47:36 +0700

    Styel: responsive for mobiles

--- src/components/Layouts/Footer/_index.tsx
+++ src/components/Layouts/Footer/_index.tsx
@@ -2,11 +2,11 @@ import { Copyright } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="dark:bg-black bg-white h-36 flex flex-col gap-y-1.5 items-center justify-center cursor-default font-semibold dark:text-gray text-black-main text-lg drop-shadow-[-50px_0_50px_rgba(0,0,0,0.25)]">
+    <footer className="dark:bg-black bg-white h-36 flex flex-col gap-y-1.5 items-center justify-center cursor-default text-sm lg:font-semibold dark:text-gray text-black-main lg:text-lg drop-shadow-[-50px_0_50px_rgba(0,0,0,0.25)]">
       <p>Released under MIT license</p>
       <p data-testid="author" className="flex items-center gap-x-0.5">
         Copyright <Copyright /> 2024-present by{" "}
-        <span className="bg-gradient-to-br from-secondary via-primary dark:to-white to-secondary inline-block text-transparent bg-clip-text font-extrabold">
+        <span className="bg-gradient-to-br from-secondary via-primary dark:to-white to-secondary inline-block text-transparent bg-clip-text lg:font-extrabold">
           DevIniter.
         </span>
       </p>
