33bc9c1eb2e34678a899e8676db01e142bf37c8b
Thanh Phong <pttp15122002@gmail.com>
2024-10-22 09:32:24 +0700

    Style: add dark background no matter light or dark mode

--- src/components/Home/Hero/Process.tsx
+++ src/components/Home/Hero/Process.tsx
@@ -2,7 +2,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Process() {
   return (
-    <div className="flex items-center justify-center w-screen lg:mt-16 mt-32">
+    <div className="flex items-center justify-center min-h-screen w-screen lg:mt-16 mt-32 bg-gray-dark dark:bg-black-main">
       <DotLottieReact
         className="lg:w-3/5 lg:max-w-6xl h-auto relative z-10"
         src="/animations/animation.lottie"
