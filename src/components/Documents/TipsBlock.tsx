78eb3865c394338baa9481eff61fd28b92998deb
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-10-27 20:00:57 +0700

    #DOC: v1.0.23 info (#20)
    * Doc: update document for installing templates
    
    * Style: use Kanit font
    
    * Doc: updae code blocks
    
    * Feat: new patch news

--- src/components/Documents/TipsBlock.tsx
+++ src/components/Documents/TipsBlock.tsx
@@ -1,9 +1,11 @@
-export default function TipsBlock({ tips }: { tips: string[] }) {
+import { ReactNode } from "react";
+
+export default function TipsBlock({ tips }: { tips: ReactNode[] }) {
   return (
     <div className="w-full rounded-2xl border-[1.5px] dark:border-white border-black mt-6 px-5 py-4">
       <p className="font-bold">🪄 Tips For You</p>
       {tips.map((tip, index) => {
-        return <p key={index}>- {tip}</p>;
+        return <p key={index}>{tip}</p>;
       })}
     </div>
   );
