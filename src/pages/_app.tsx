7e10bfd8337ad800267f6a1beea8c6010a7f3e44
Thanh Phong <pttp15122002@gmail.com>
2024-10-30 07:00:43 +0700

    Feat: add SppedInsights component to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
 import "@/styles/nextra.css";
 import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
