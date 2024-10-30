67084fd19c3ef7f58e512ac492dc30c3587e5555
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-10-30 07:10:58 +0700

    #FEAT: Vercel analytics (#24)
    * Chore: install vercel analytics
    
    * Feat: add analytics component to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import "@/styles/nextra.css";
 import type { AppProps } from "next/app";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
