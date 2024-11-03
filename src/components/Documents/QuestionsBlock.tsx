d30c6fc746e45798342cd493bf254ad5e9ce82da
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-11-03 18:59:33 +0700

    Fix: white text color on light mode (#34)

--- src/components/Documents/QuestionsBlock.tsx
+++ src/components/Documents/QuestionsBlock.tsx
@@ -17,14 +17,12 @@ export default function QuestionBlock({ questions }: QuestionBlockProps) {
       <div className="dark:bg-[#131412] bg-gray-light h-4/5 px-5 py-4 flex flex-col gap-y-2.5 rounded-b-2xl">
         {questions.map((q, index) => (
           <div key={index} className="mb-4 last:mb-0">
-            <h4 className="text-white font-semibold">{q.question}</h4>
-            {q.description && (
-              <p className="text-gray-300 text-sm mt-1">{q.description}</p>
-            )}
+            <h4 className="font-semibold">{q.question}</h4>
+            {q.description && <p className="text-sm mt-1">{q.description}</p>}
             {q.options && (
               <ul className="mt-2 ml-4">
                 {q.options.map((opt, idx) => (
-                  <li key={idx} className="text-gray-300 text-sm">
+                  <li key={idx} className="text-sm">
                     - {opt}
                   </li>
                 ))}
