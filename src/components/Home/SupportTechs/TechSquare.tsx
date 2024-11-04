88ed7b38fcfca21659668508f29b2b2947b2da8b
Thanh Phong <pttp15122002@gmail.com>
2024-11-04 12:51:55 +0700

    Refactor: use TechProps and add mousemove trigger

--- src/components/Home/SupportTechs/TechSquare.tsx
+++ src/components/Home/SupportTechs/TechSquare.tsx
@@ -1,26 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
-
-type Props = {
-  name: string;
-  href: string;
-  logoURL: string;
-  shadowColor: string;
-};
+import { TechProps } from "./_index";
+import { MouseEventHandler } from "react";
 
 export default function TechSquare({
-  name,
-  href,
-  logoURL,
-  shadowColor,
-}: Props) {
+  item,
+  onMouseMove,
+}: {
+  item: TechProps;
+  onMouseMove: MouseEventHandler<HTMLAnchorElement>;
+}) {
+  const { href, name, logoURL, shadowColor } = item;
   return (
-    <Link href={href} target="_blank">
+    <Link onMouseMove={onMouseMove} href={href} target="_blank">
       <button
         className={`group/${name} size-16 lg:size-24 bg-white dark:bg-gray-dark lg:hover:drop-shadow-lg ${shadowColor} rounded-lg p-4 flex items-center justify-center shadow-xl transition-transform duration-300 hover:scale-105`}
       >
         <Image
-          src={logoURL}
+          src={"/images/techstacks/" + logoURL}
           alt={name}
           width={0}
           height={0}
