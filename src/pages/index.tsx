aaeb4e21f6c1587074ce16667212ad6388269cca
Thanh Phong <pttp15122002@gmail.com>
2024-11-04 11:01:20 +0700

    Feat: use lazy load for multiple components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import { Hero } from "@/components/Home/_index";
-import Highlight from "@/components/Home/Highlight/_index";
-import SupportedTechs from "@/components/Home/SupportTechs/_index";
-import TemplateDemos from "@/components/Home/TemplateDemos";
 import Template from "@/components/Layouts/Template";
+import dynamic from "next/dynamic";
+
+const DynamicSupportedTechs = dynamic(
+  () => import("../components/Home/SupportTechs/_index"),
+  {
+    loading: () => <></>,
+    ssr: false,
+  }
+);
+
+const DynamicTemplateDemos = dynamic(
+  () => import("../components/Home/TemplateDemos/index"),
+  {
+    loading: () => <></>,
+    ssr: false,
+  }
+);
+
+const DynamicHighlightSection = dynamic(
+  () => import("../components/Home/Highlight/_index"),
+  {
+    loading: () => <></>,
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
@@ -13,9 +35,9 @@ export default function Home() {
     >
       <Hero />
       <section className="w-screen lg:max-w-7xl min-h-screen mx-auto flex flex-col items-center mt-24 lg:mt-48 lg:mb-24 mb-16">
-        <Highlight />
-        <SupportedTechs />
-        <TemplateDemos />
+        <DynamicHighlightSection />
+        <DynamicSupportedTechs />
+        <DynamicTemplateDemos />
       </section>
     </Template>
   );
