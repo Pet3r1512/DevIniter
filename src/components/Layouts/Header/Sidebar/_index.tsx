c5fa5eb0d84f4cbd66bcaf8748b34d1a42ae48cd
Thanh Phong <pttp15122002@gmail.com>
2024-11-05 15:47:42 +0700

    Feat: scroll to demo section on mobile's sidebar

--- src/components/Layouts/Header/Sidebar/_index.tsx
+++ src/components/Layouts/Header/Sidebar/_index.tsx
@@ -1,12 +1,16 @@
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { Menu } from "lucide-react";
-import { navs } from "../Navigations";
+import { BadgePattern, navs } from "../Navigations";
 import Link from "next/link";
 import Logo from "../Logo";
 import ThemeToggle from "../../ThemeToggle";
 import Github from "../Github";
 
 export default function Sidebar() {
+  const handleNavClick = () => {
+    document.getElementById("sidebar-trigger")?.click();
+  };
+
   return (
     <Drawer direction="left">
       <DrawerTrigger id="sidebar-trigger" className="lg:hidden w-1/3">
@@ -17,8 +21,14 @@ export default function Sidebar() {
         <aside className="flex flex-col gap-y-2.5 px-5">
           {navs.map((nav, index) => {
             return (
-              <Link key={index} href={nav.href} className="font-semibold">
+              <Link
+                key={index}
+                onClick={() => handleNavClick()}
+                href={nav.href}
+                className="font-semibold"
+              >
                 {nav.name}
+                {nav.status && BadgePattern[nav.status]}
               </Link>
             );
           })}
