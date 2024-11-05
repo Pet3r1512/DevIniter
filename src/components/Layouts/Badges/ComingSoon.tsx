4411a711e3d77872cb68dec4174e0ec3f4677cfb
Thanh Phong <pttp15122002@gmail.com>
2024-11-05 14:49:54 +0700

    Feat: coming soon badge

---
+++ src/components/Layouts/Badges/ComingSoon.tsx
@@ -0,0 +1,7 @@
+export default function ComingSoon() {
+  return (
+    <span className="w-fit !text-[8.5px] bg-[#e0ac00] dark:bg-[#e09b31] text-white rounded-lg px-1 pt-[0.9px] text-center">
+      Coming
+    </span>
+  );
+}
