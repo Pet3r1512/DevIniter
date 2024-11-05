521879f8a6378d4971230871dadb533794a1a453
Thanh Phong <pttp15122002@gmail.com>
2024-11-05 15:53:18 +0700

    Style: fixed bage text size to 8.5px

--- src/components/Layouts/Badges/NewBadge.tsx
+++ src/components/Layouts/Badges/NewBadge.tsx
@@ -4,7 +4,7 @@ export default function NewBadge({ className }: { className?: string }) {
   return (
     <span
       className={cn(
-        "w-fit text-[8.5px] bg-[#008000] text-white rounded-lg px-1 pt-[0.9px] text-center",
+        "w-fit !text-[8.5px] bg-[#008000] text-white rounded-lg px-1 pt-[0.9px] text-center",
         className
       )}
     >
