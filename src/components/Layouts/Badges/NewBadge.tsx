d9b774c657dda7d9900fcbb16a7ccdd7beac23c1
Thanh Phong <pttp15122002@gmail.com>
2024-11-05 15:46:52 +0700

    Feat: allow to pass new classname to badges

--- src/components/Layouts/Badges/NewBadge.tsx
+++ src/components/Layouts/Badges/NewBadge.tsx
@@ -1,6 +1,13 @@
-export default function NewBadge() {
+import { cn } from "@/utils/utils";
+
+export default function NewBadge({ className }: { className?: string }) {
   return (
-    <span className="w-fit !text-[8.5px] bg-[#008000] text-white rounded-lg px-1 pt-[0.9px] text-center">
+    <span
+      className={cn(
+        "w-fit text-[8.5px] bg-[#008000] text-white rounded-lg px-1 pt-[0.9px] text-center",
+        className
+      )}
+    >
       New
     </span>
   );
