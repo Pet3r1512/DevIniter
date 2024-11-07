5349b16277be409aa073edab2775004410e89526
Thanh Phong <pttp15122002@gmail.com>
2024-11-07 10:34:44 +0700

    Chore: increase delay time

--- src/components/Home/TemplateDemos/index.tsx
+++ src/components/Home/TemplateDemos/index.tsx
@@ -117,7 +117,7 @@ export default function TemplateDemos() {
       style: {
         transform: isInView ? "none" : "translateX(-200px)",
         opacity: isInView ? 1 : 0,
-        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.85s",
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1.35s",
       },
     },
     {
@@ -215,7 +215,7 @@ export default function TemplateDemos() {
       style: {
         transform: isInView ? "none" : "translateX(200px)",
         opacity: isInView ? 1 : 0,
-        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.85s",
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1.35s",
       },
     },
   ];
