da4a14a7e3e741a2718d155696979fcc4b50afe7
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-11-07 10:42:33 +0700

    #HOTFIX: Nextra config and favicon (#52)
    * Chore: remove unused
    
    * Chore: use favicon inside favicon folder instead

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,34 +3,6 @@ import Logo from "@/components/Layouts/Header/Logo";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const themeConfig: DocsThemeConfig = {
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="DevIniter" />
-      <meta property="og:description" content="Ultimate Starter Tempaltes" />
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <title>DevIniter Docs | Blazing Starter Templates</title>
-    </>
-  ),
-
   logo: <Logo />,
   project: {
     link: "https://github.com/Pet3r1512/DevIniter",
