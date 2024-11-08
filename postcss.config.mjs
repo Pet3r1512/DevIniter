49245383e807aa3d7a91c146679dba0dc4ceea30
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-11-08 16:02:12 +0700

    Setup/purgecss for postcss (#56)
    * Install postcss-purgecss
    
    * Config for postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,7 +3,21 @@ const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          "@fullhuman/postcss-purgecss": {
+            content: ["./**/*.{tsx,mdx}"],
+            css: ["./src/styles/globals.css", "./src/styles/nextra.css"],
+            safelist: [
+              /data-theme$/, // for theme classes
+              /^nextra-/, // for nextra-specific classes
+            ],
+          },
+          cssnano: {
+            preset: "default",
+          },
+        }
+      : {}),
   },
 };
 
