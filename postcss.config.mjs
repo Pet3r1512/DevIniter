3e395eaca6c89877cc08983a3a5db10484f6b7b0
Thanh Phong <pttp15122002@gmail.com>
2024-11-08 17:21:41 +0700

    Feat: try use purgecss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,6 +5,14 @@ const config = {
     autoprefixer: {},
     ...(process.env.NODE_ENV === "production"
       ? {
+          "@fullhuman/postcss-purgecss": {
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
           cssnano: {
             preset: [
               "default",
