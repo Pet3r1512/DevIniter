ea4ac811aa66ac3457aab952e7a02c315b5d6498
Thanh Phong <pttp15122002@gmail.com>
2024-11-08 10:15:11 +0700

    Fix: incorrect DOM properties

--- src/components/Home/SectionTitle.tsx
+++ src/components/Home/SectionTitle.tsx
@@ -21,9 +21,9 @@ export default function SectionTitle({
       }}
       className="flex flex-col gap-y-5 items-center relative z-10"
     >
-      <p className="bg-gradient-to-br from-secondary via-primary dark:to-white to-secondary inline-block text-transparent bg-clip-text text-3xl md:text-4xl lg:text-5xl font-bold text-center">
+      <div className="bg-gradient-to-br from-secondary via-primary dark:to-white to-secondary inline-block text-transparent bg-clip-text text-3xl md:text-4xl lg:text-5xl font-bold text-center">
         {title}
-      </p>
+      </div>
       <p className="dark:text-gray-light md:text-lg w-[85vw] text-center lg:text-xl">
         {subtitle}
       </p>
