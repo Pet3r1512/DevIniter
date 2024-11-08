570474231c321c74e1832293370880b66f9dd89c
Thanh Phong <pttp15122002@gmail.com>
2024-11-08 17:50:47 +0700

    Add mdx ext

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -9,9 +9,9 @@ module.exports = {
       ? [
           purgecss({
             content: [
-              "./pages/**/*.{js,ts,jsx,tsx}",
-              "./components/**/*.{js,ts,jsx,tsx}",
-              "./src/**/*.{js,ts,jsx,tsx}",
+              "./pages/**/*.{js,ts,jsx,tsx, mdx}",
+              "./components/**/*.{js,ts,jsx,tsx, mdx}",
+              "./src/**/*.{js,ts,jsx,tsx, mdx}",
             ],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
