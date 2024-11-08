401e400a89bf50b190f47a98cc77fc87114880e5
Thanh Phong <pttp15122002@gmail.com>
2024-11-08 17:15:11 +0700

    Imp: update postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,7 +3,20 @@ const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          cssnano: {
+            preset: [
+              "default",
+              {
+                discardComments: {
+                  removeAll: true,
+                },
+              },
+            ],
+          },
+        }
+      : {}),
   },
 };
 
