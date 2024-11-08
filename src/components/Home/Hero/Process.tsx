ed08c080e0736d581e01f0dc123f3e912a1e4941
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-11-08 09:51:11 +0700

    #IMP-015: setup bundle analyzer with lazy loading (#53)
    * Feat: config for bundle analyzer
    
    * Chore: install bundle-analyzer and webpacl-bundle-analyzer
    
    * Chore: ignore analyze result folder
    
    * Chore: eslint ignore anonymous export
    
    * Feat: lazy load tech's logos
    
    * Feat: lazy load animation

--- src/components/Home/Hero/Process.tsx
+++ src/components/Home/Hero/Process.tsx
@@ -1,9 +1,18 @@
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+"use client";
+import dynamic from "next/dynamic";
+
+const DotLottiesReact = dynamic(
+  () =>
+    import("@lottiefiles/dotlottie-react").then((mod) => mod.DotLottieReact),
+  {
+    ssr: false, //Disable server-side rendering
+  }
+);
 
 export default function Process() {
   return (
     <div className="flex items-center justify-center min-h-screen w-screen lg:mt-16 mt-32 bg-gray-dark dark:bg-black-main">
-      <DotLottieReact
+      <DotLottiesReact
         className="lg:w-3/5 lg:max-w-6xl h-auto relative z-10"
         src="/animations/animation.lottie"
         loop
