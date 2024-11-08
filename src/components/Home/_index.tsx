8505d5c0d0e155af685d245563b8919f935d08c4
Thanh Phong <pttp15122002@gmail.com>
2024-11-08 10:14:37 +0700

    Feat: only load lottie when hero section is in view

--- src/components/Home/_index.tsx
+++ src/components/Home/_index.tsx
@@ -1,15 +1,20 @@
+"use client";
+
+import { useInViewStore } from "@/stores/useInViewStore";
 import Introduction from "./Hero/Introduction";
 import dynamic from "next/dynamic";
 
 const DynamicAnimation = dynamic(() => import("../Home/Hero/Process"), {
   loading: () => <></>,
+  ssr: false,
 });
 
 export function Hero() {
+  const { isHeroInView } = useInViewStore();
   return (
     <section className="bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 min-h-screen">
       <Introduction />
-      <DynamicAnimation />
+      {isHeroInView && <DynamicAnimation />}
     </section>
   );
 }
