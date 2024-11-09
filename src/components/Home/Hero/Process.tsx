9d3288baa55f7fe43650617169e5e44ad46166bc
Thanh Phong <pttp15122002@gmail.com>
2024-11-09 16:15:35 +0700

    Feat: change stored value whenever animation is in and out view

--- src/components/Home/Hero/Process.tsx
+++ src/components/Home/Hero/Process.tsx
@@ -1,5 +1,8 @@
 "use client";
+import { useInViewStore } from "@/stores/useInViewStore";
+import { useInView } from "framer-motion";
 import dynamic from "next/dynamic";
+import { useEffect, useRef } from "react";
 
 const DotLottiesReact = dynamic(
   () =>
@@ -10,8 +13,19 @@ const DotLottiesReact = dynamic(
 );
 
 export default function Process() {
+  const { setAnimationInView } = useInViewStore();
+  const ref = useRef(null);
+  const isAnimationInView = useInView(ref);
+
+  useEffect(() => {
+    setAnimationInView(isAnimationInView);
+  }, [isAnimationInView, setAnimationInView]);
+
   return (
-    <div className="flex items-center justify-center min-h-screen w-screen lg:mt-16 mt-32 bg-gray-dark dark:bg-black-main">
+    <div
+      ref={ref}
+      className="flex items-center justify-center min-h-screen w-screen lg:mt-16 mt-32 bg-gray-dark dark:bg-black-main"
+    >
       <DotLottiesReact
         className="lg:w-3/5 lg:max-w-6xl h-auto relative z-10"
         src="/animations/animation.lottie"
