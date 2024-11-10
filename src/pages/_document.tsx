f0a44792ea09ba47e357e8cce965c2e74b52341c
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-11-10 22:10:52 +0700

    Fix: move all link tags into next's Head component (#66)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,34 +3,35 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
-      ></meta>
-      <meta
-        name="description"
-        content="The Ultimate Starter Templates for Developers"
-      />
-      <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        ></meta>
+        <meta
+          name="description"
+          content="The Ultimate Starter Templates for Developers"
+        />
+        <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
