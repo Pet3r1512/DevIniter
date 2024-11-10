f283e3942049884c836bc58e08a3a905e3752e7b
Thanh Phong <pttp15122002@gmail.com>
2024-11-10 16:18:14 +0700

    Style: add some space at top of section

--- src/components/Home/Highlight/_index.tsx
+++ src/components/Home/Highlight/_index.tsx
@@ -3,7 +3,7 @@ import BentoGrid from "./BentoGrid";
 
 export default function Highlight() {
   return (
-    <section className="flex flex-col gap-y-24 lg:gap-y-56 justify-center min-h-screen">
+    <section className="flex flex-col gap-y-24 lg:gap-y-56 justify-center min-h-screen lg:mt-32">
       <SectionTitle
         title={
           <p>
