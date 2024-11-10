976070d4bf05ab63e12e0b8f4f49514ed9c8f282
Thanh Phong <pttp15122002@gmail.com>
2024-11-10 15:01:21 +0700

    Style: add animation

--- src/components/Home/_index.tsx
+++ src/components/Home/_index.tsx
@@ -1,20 +1,12 @@
-"use client";
-
-import { useInViewStore } from "@/stores/useInViewStore";
+import { BackgroundLines } from "../ui/aceternity/background-lines";
 import Introduction from "./Hero/Introduction";
-import dynamic from "next/dynamic";
-
-const DynamicAnimation = dynamic(() => import("../Home/Hero/Process"), {
-  loading: () => <></>,
-  ssr: false,
-});
 
 export function Hero() {
-  const { isHeroInView } = useInViewStore();
   return (
     <section className="bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 min-h-screen">
-      <Introduction />
-      {isHeroInView && <DynamicAnimation />}
+      <BackgroundLines>
+        <Introduction />
+      </BackgroundLines>
     </section>
   );
 }
