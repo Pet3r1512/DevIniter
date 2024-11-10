93dba3e4b25f0b3fec2a009711966112f65c49e7
Thanh Phong <pttp15122002@gmail.com>
2024-11-10 11:46:45 +0700

    Style: make badge become absolute

--- src/pages/docs/_meta.js
+++ src/pages/docs/_meta.js
@@ -7,9 +7,9 @@ export default {
   installation: "Installation",
   template_structure: {
     title: (
-      <div className="flex items-center w-full justify-between">
+      <div className="flex items-center w-full justify-between relative">
         <p>Template Structure</p>
-        <NewBadge />
+        <NewBadge className="absolute -top-4 -right-14" />
       </div>
     ),
   },
