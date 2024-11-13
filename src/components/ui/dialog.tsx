6eac48193049ee66593ce4b508bedd305ba5764e
Thanh Phong <pttp15122002@gmail.com>
2024-11-13 15:55:50 +0700

    Fixbug

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -28,8 +28,10 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
+    title: string | React.ReactNode;
+  }
+>(({ className, children, title, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -40,6 +42,7 @@ const DialogContent = React.forwardRef<
       )}
       {...props}
     >
+      <DialogTitle className="sr-only">{title}</DialogTitle>
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-neutral-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-neutral-100 data-[state=open]:text-neutral-500 dark:ring-offset-neutral-950 dark:focus:ring-neutral-300 dark:data-[state=open]:bg-neutral-800 dark:data-[state=open]:text-neutral-400">
         <Cross2Icon className="h-4 w-4" />
