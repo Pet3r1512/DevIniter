0e8751e757e648643c0cb1e65c9621584e79703a
Thanh Phong <pttp15122002@gmail.com>
2024-11-13 22:06:00 +0700

    Style: add fixed height on mobile

--- src/components/Layouts/PatchNews.tsx
+++ src/components/Layouts/PatchNews.tsx
@@ -4,7 +4,7 @@ export default function PatchNews() {
   return (
     <div
       data-testid
-      className="bg-gradient-to-r text-white from-primary via-primary-light to-secondary h-fit py-1.5 lg:h-12 w-full flex flex-col lg:flex-row text-center gap-x-1 items-center justify-center font-semibold leading-normal"
+      className="bg-gradient-to-r text-white from-primary via-primary-light to-secondary h-24 py-1.5 lg:h-12 w-full flex flex-col lg:flex-row text-center gap-x-1 items-center justify-center font-semibold leading-normal"
     >
       <p>
         🚀 DevIniter <span className="text-gold">v1.2.2</span> has been released
