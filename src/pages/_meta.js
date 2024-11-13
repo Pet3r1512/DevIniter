b73a8d8561a5577b3f8ca5c424461fd7fd61109d
Thanh Phong <pttp15122002@gmail.com>
2024-11-13 15:30:56 +0700

    Feat: add features overview with new badge

--- src/pages/_meta.js
+++ src/pages/_meta.js
@@ -1,6 +1,16 @@
+import NewBadge from "../components/Layouts/Badges/NewBadge";
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   docs: "Documentation",
-  "free-for-devs": "Free For Devs",
+  features: {
+    title: (
+      <div className="flex items-center w-full justify-between relative">
+        <p>Features Overview</p>
+        <NewBadge />
+      </div>
+    ),
+  },
   release: "Release Notes",
+  "free-for-devs": "Free For Devs",
 };
