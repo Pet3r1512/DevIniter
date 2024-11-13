a8b579835ce412f2b3b04e5b9aff1b1a84c2943b
Thanh Phong <pttp15122002@gmail.com>
2024-11-13 08:04:23 +0700

    Feat: use new navbar

--- src/components/Layouts/Header/_index.tsx
+++ src/components/Layouts/Header/_index.tsx
@@ -1,9 +1,9 @@
 import Github from "./Github";
 import Logo from "./Logo";
-import Navigation from "./Navigations";
 import SearchBar from "./SearchBar/_index";
 import ThemeToggle from "../ThemeToggle";
 import Sidebar from "./Sidebar/_index";
+import { Navbar } from "./Navbar";
 
 export default function Header() {
   return (
@@ -12,7 +12,7 @@ export default function Header() {
         <Sidebar />
         <div className="flex items-center gap-x-8">
           <Logo />
-          <Navigation />
+          <Navbar />
         </div>
         <div className="flex items-center gap-x-5 lg:hidden w-1/3 justify-end">
           <SearchBar />
