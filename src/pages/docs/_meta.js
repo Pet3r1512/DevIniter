5860a5f6654ad15a554d048d6eb4e2d7d1b18b63
Thanh Phong <pttp15122002@gmail.com>
2024-11-13 15:31:35 +0700

    Style: remove new badge

--- src/pages/docs/_meta.js
+++ src/pages/docs/_meta.js
@@ -1,16 +1,7 @@
-import NewBadge from "../../components/Layouts/Badges/NewBadge";
-
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   introduction: "Introduction",
   get_started: "Getting Started",
   installation: "Installation",
-  template_structure: {
-    title: (
-      <div className="flex items-center w-full justify-between relative">
-        <p>Template Structure</p>
-        <NewBadge className="md:absolute md:-top-4 md:-right-14" />
-      </div>
-    ),
-  },
+  template_structure: "Template Structure",
 };
