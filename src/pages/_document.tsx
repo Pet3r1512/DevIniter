f687df918c3de33f427178baf3fca854de46663f
Thanh Phong <pttp15122002@gmail.com>
2024-11-15 14:11:38 +0700

    Fix: missing favicon on prod

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,30 +8,6 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
         ></meta>
-        <meta
-          name="description"
-          content="The Ultimate Starter Templates for Developers"
-        />
-        <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body className="antialiased">
         <Main />
