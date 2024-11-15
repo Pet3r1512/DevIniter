bc78346f34b77c7692c53cdad62344169c9a3ab5
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-11-15 14:16:39 +0700

    Fix: missing favicon on prod (#92)

--- src/components/Layouts/Template.tsx
+++ src/components/Layouts/Template.tsx
@@ -28,6 +28,30 @@ export default function Template({
     <>
       <Head>
         <title>{fullPageName}</title>
+        <meta
+          name="description"
+          content="The Ultimate Starter Templates for Developers"
+        />
+        <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <ReactLenis root>
         <main className={cn("scrollbar-hide", kanit.className)}>
