2a02da3f0f6a27d0b0247f8a534c039fb43e6bbd
Thanh Phong <pttp15122002@gmail.com>
2024-11-17 18:23:47 +0700

    Feat: use dynamic import on hero section

--- src/components/Home/_index.tsx
+++ src/components/Home/_index.tsx
@@ -1,6 +1,6 @@
 import Introduction from "./Hero/Introduction";
 
-export function Hero() {
+export default function Hero() {
   return (
     <section className="bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 min-h-[calc(100vh-22.5rem)] lg:min-h-[85dvh]">
       <Introduction />
