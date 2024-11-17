3e9b6dcec56949a946428e1182475e211cb6829c
Thanh Phong <pttp15122002@gmail.com>
2024-11-17 18:20:39 +0700

    Test: remove unused

--- src/components/Layouts/Header/header.test.tsx
+++ src/components/Layouts/Header/header.test.tsx
@@ -9,27 +9,4 @@ describe("Render Header", () => {
       expect(tree).toMatchSnapshot();
     });
   });
-
-  // it("Should contain correct Header content",async () => {
-  //   const tree = render(<Header />);
-
-  //   waitFor(() => {
-  //     expect(logo).toBeInTheDocument();
-  //   })
-
-  //   const logo = tree.getByTestId("Logo");
-  //   expect(logo).toBeInTheDocument();
-
-  //   const navbar = tree.getByTestId("navbar");
-  //   expect(navbar).toBeInTheDocument();
-
-  //   const theme_togger = tree.getByTestId("theme-togger");
-  //   expect(theme_togger).toBeInTheDocument();
-
-  //   const verticalLine = tree.getByTestId("vt");
-  //   expect(verticalLine).toBeInTheDocument();
-
-  //   const github = tree.getByTestId("github-logo");
-  //   expect(github).toBeInTheDocument();
-  // });
 });
