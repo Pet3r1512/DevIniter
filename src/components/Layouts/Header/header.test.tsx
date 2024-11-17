df0e7a673099fcc78c7c916d54f0c75a8875d944
Thanh Phong <pttp15122002@gmail.com>
2024-11-17 15:21:13 +0700

    Feat: use wairFor to wait component to be loaded

--- src/components/Layouts/Header/header.test.tsx
+++ src/components/Layouts/Header/header.test.tsx
@@ -1,11 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import Header from "./_index";
 
 describe("Render Header", () => {
-  it("Should match snapshot", () => {
+  it("Should match snapshot", async () => {
     const tree = render(<Header />);
-    expect(tree).toMatchSnapshot();
+    await waitFor(() => {
+      expect(tree).toMatchSnapshot();
+    });
   });
 
   it("Should contain correct Header content", () => {
