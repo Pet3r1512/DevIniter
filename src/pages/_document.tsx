808df7669aa642e1d3bc618cb8a69df8b76a3481
Thanh Phong <pttp15122002@gmail.com>
2024-11-18 09:37:38 +0700

    Fix: unused viewport tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
-        ></meta>
-      </Head>
+      <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
