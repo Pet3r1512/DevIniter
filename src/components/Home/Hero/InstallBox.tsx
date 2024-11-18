26f3cda36cbfd602c9fd8a14a5d3c04d02c8dd0f
Thanh Phong <pttp15122002@gmail.com>
2024-11-18 09:37:22 +0700

    Refactor: split component

--- src/components/Home/Hero/InstallBox.tsx
+++ src/components/Home/Hero/InstallBox.tsx
@@ -1,46 +1,7 @@
 import { BorderTrail } from "@/components/ui/aceternity/border-trail";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Clipboard, ClipboardCheck } from "lucide-react";
-import { useState, useEffect } from "react";
+import PackageManagerPopup from "./PackageManagerPopup";
 
 export default function InstallBox() {
-  const [isCopied, setIsCopied] = useState(false);
-  const [open, setOpen] = useState(false);
-  const packageManagers = [
-    {
-      name: "npm",
-      cmd: "npx @deviniter/cli@latest",
-    },
-    {
-      name: "pnpm",
-      cmd: "pnpm dlx @deviniter/cli@latest",
-    },
-  ];
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      const popover = document.getElementById("popover");
-      const trigger = document.getElementById("popover-trigger");
-      if (
-        popover &&
-        !popover.contains(event.target as Node) &&
-        trigger &&
-        !trigger.contains(event.target as Node)
-      ) {
-        setOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
   return (
     <div className="flex items-center justify-center rounded-lg md:rounded-xl lg:rounded-2xl p-2 md:p-3 lg:p-5 relative !dark:border-black !border-white dark:bg-black bg-white gap-x-2.5 h-10 w-52 md:h-11 md:w-60 lg:h-16 lg:w-80">
       <BorderTrail
@@ -53,38 +14,7 @@ export default function InstallBox() {
       <p className="text-sm md:text-base lg:text-xl">
         npx @deviniter/cli@latest
       </p>
-      <Popover open={open}>
-        <PopoverTrigger
-          id="popover-trigger"
-          onClick={() => {
-            setOpen(true);
-          }}
-          className="transition-all duration-200 ease-linear"
-        >
-          {!isCopied ? (
-            <Clipboard className="size-3.5 md:size-5 lg:size-6" />
-          ) : (
-            <ClipboardCheck className="text-secondary size-3.5 md:size-5 lg:size-6" />
-          )}
-        </PopoverTrigger>
-        <PopoverContent id="popover" className="flex flex-col gap-y-1.5 w-fit">
-          {packageManagers.map((pkg, index) => {
-            return (
-              <p
-                onClick={() => {
-                  navigator.clipboard.writeText(pkg.cmd);
-                  setIsCopied(true);
-                  setOpen(false);
-                }}
-                key={index}
-                className="cursor-pointer lg:hover:text-primary transition-all duration-150 ease-linear"
-              >
-                {pkg.name}
-              </p>
-            );
-          })}
-        </PopoverContent>
-      </Popover>
+      <PackageManagerPopup />
     </div>
   );
 }
