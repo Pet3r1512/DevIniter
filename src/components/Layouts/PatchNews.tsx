05d36173c9f7b149bad34e067c27746b1f4f834d
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-11-19 13:54:45 +0700

    #DOC: update v1.2.11 (#103)
    * Chore: update banner
    
    * Doc: add release note for v1.2.10 & v1.2.11

--- src/components/Layouts/PatchNews.tsx
+++ src/components/Layouts/PatchNews.tsx
@@ -7,8 +7,8 @@ export default function PatchNews() {
       className="bg-gradient-to-r text-white from-primary via-primary-light to-secondary h-24 py-1.5 lg:h-12 w-full flex flex-col lg:flex-row text-center gap-x-1 items-center justify-center font-semibold leading-normal"
     >
       <p>
-        🚀 DevIniter <span className="text-gold">v1.2.9</span> has been released
-        with NextJS and Vite template available.
+        🚀 DevIniter <span className="text-gold">v1.2.11</span> has been
+        released with NextJS and Vite template available.
       </p>
       <Link
         className="underline decoration-solid text-gold"
