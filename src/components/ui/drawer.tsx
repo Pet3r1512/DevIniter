e818d69f6a3127279af75acd2ccf0a5e59f081b7
Peter Pham <pttp15122002@gmail.com>
2024-11-22 07:19:58 +0700

    style: remove unused border radius

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -40,7 +40,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-r-[18px] border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950",
         className
       )}
       {...props}
