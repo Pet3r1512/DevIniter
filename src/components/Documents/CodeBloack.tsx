91c9bf7d3b3db577c27ded186a193dcdbb20fe28
Peter Pham <pttp15122002@gmail.com>
2024-11-26 16:25:53 +0700

    feat: code highlighter from shiki

---
+++ src/components/Documents/CodeBloack.tsx
@@ -0,0 +1,41 @@
+import { useTheme } from "@/hooks/useTheme";
+import { useEffect, useState } from "react";
+import { codeToHtml } from "shiki";
+
+export default function CodeBlock({
+  code,
+  lang,
+}: {
+  code: string;
+  lang: string;
+}) {
+  const { theme } = useTheme();
+  const [htmlOutput, setHtmlOutput] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCode = async () => {
+      try {
+        const out = await codeToHtml(code, {
+          lang: lang || "plaintext",
+          themes: {
+            dark: "houston",
+            light: "one-light",
+          },
+        });
+        setHtmlOutput(out);
+      } catch (error) {
+        console.error("Failed to generate code block HTML:", error);
+        setHtmlOutput(null);
+      }
+    };
+
+    fetchCode();
+  }, [code, theme, lang]);
+
+  return (
+    <div
+      className="my-5 shiki-codeblock"
+      dangerouslySetInnerHTML={{ __html: htmlOutput || "" }}
+    />
+  );
+}
