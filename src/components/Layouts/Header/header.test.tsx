2835eefb26d08a9908b23bfe26813b05e9085339
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-11-26 07:14:28 +0700

    #IMP-035: update unit tests (#114)
    * chore: add testid for header
    
    * test: write test for header's skeleton
    
    * test: test real header element after loading
    
    * test: update header's snapshot

--- src/components/Layouts/Header/header.test.tsx
+++ src/components/Layouts/Header/header.test.tsx
@@ -1,12 +1,18 @@
-import { render, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import Header from "./_index";
 
 describe("Render Header", () => {
-  it("Should match snapshot", async () => {
-    const tree = render(<Header />);
-    await waitFor(() => {
-      expect(tree).toMatchSnapshot();
-    });
+  it("Should render a skeleton", async () => {
+    const { asFragment } = render(<Header />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("Should render Header after loading", async () => {
+    const { asFragment } = render(<Header />);
+
+    await waitFor(() => screen.getByTestId("header"));
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
