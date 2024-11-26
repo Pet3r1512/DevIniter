16310e30f7b488767ae6e040c46f30629dfa81a0
Peter Pham <pttp15122002@gmail.com>
2024-11-26 11:40:58 +0700

    doc: desktop's banner

--- src/components/Layouts/PatchNews.tsx
+++ src/components/Layouts/PatchNews.tsx
@@ -7,9 +7,8 @@ export default function PatchNews() {
       className="bg-gradient-to-r text-white from-primary via-primary-light to-secondary h-8 py-1.5 lg:h-12 w-full flex flex-row text-center gap-x-1 items-center justify-center text-sm md:text-base font-semibold leading-normal"
     >
       <p className="hidden md:block">
-        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.14</span>{" "}
-        now lets users choose whether to install Prisma when selecting the
-        Next.js template.
+        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.16</span>{" "}
+        has been released with Next15
       </p>
       <p className="md:hidden">
         🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.14</span>{" "}
