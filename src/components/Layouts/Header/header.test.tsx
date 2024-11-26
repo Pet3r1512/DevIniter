974eba276bbdeee4801acc2e4c0c041bcc111c62
Peter Pham <pttp15122002@gmail.com>
2024-11-26 07:09:53 +0700

    test: test real header element after loading

--- src/components/Layouts/Header/header.test.tsx
+++ src/components/Layouts/Header/header.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import Header from "./_index";
 
@@ -8,5 +8,11 @@ describe("Render Header", () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+  it("Should render Header after loading", async () => {
+    const { asFragment } = render(<Header />);
+
+    await waitFor(() => screen.getByTestId("header"));
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
