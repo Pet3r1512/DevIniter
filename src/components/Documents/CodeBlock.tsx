00257655f6d9e2f19950d74612de70dff3741d25
Peter Pham <pttp15122002@gmail.com>
2024-11-27 15:48:34 +0700

    feat: expand codeblock for filename

--- src/components/Documents/CodeBlock.tsx
+++ src/components/Documents/CodeBlock.tsx
@@ -35,9 +35,16 @@ export default function CodeBlock({
   }, [code, theme, lang]);
 
   return (
-    <div
-      className="my-5 shiki-codeblock"
-      dangerouslySetInnerHTML={{ __html: htmlOutput || "" }}
-    />
+    <div className="shiki-container dark:bg-[#17191e] bg-[#fff] rounded-2xl my-5 p-5">
+      {filename && (
+        <p className="cursor-default text-gray-500 font-semibold italic select-none">
+          {filename}
+        </p>
+      )}
+      <div
+        className="shiki-codeblock"
+        dangerouslySetInnerHTML={{ __html: htmlOutput || "" }}
+      />
+    </div>
   );
 }
