62f537b33bb22006a0175bcd298b226bc014ee9e
Peter Pham <pttp15122002@gmail.com>
2024-11-28 15:40:21 +0700

    doc: v1.2.17 news

--- src/components/Layouts/PatchNews.tsx
+++ src/components/Layouts/PatchNews.tsx
@@ -7,11 +7,11 @@ export default function PatchNews() {
       className="bg-gradient-to-r text-white from-primary via-primary-light to-secondary h-8 py-1.5 lg:h-12 w-full flex flex-row text-center gap-x-1 items-center justify-center text-sm md:text-base font-semibold leading-normal"
     >
       <p className="hidden md:block">
-        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.16</span>{" "}
+        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.17</span>{" "}
         has been released with Next15
       </p>
       <p className="md:hidden">
-        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.16</span>{" "}
+        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.17</span>{" "}
         is out now.
       </p>
       <Link
