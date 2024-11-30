eb2f9a930f66b2b7e68e037a0181457a3602ac03
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-11-30 17:33:37 +0700

    #DOC-014: Yarn v4.0.0 supported (#123)
    * chore: add yarn with yarn's logo
    
    * feat: add yarnCommand as a string param
    
    * doc: update v1.2.19 with yarn
    
    * doc: add yarn commands for installation documentation
    
    * doc: add yarn command for prisma
    
    * doc: add changelog from v1.2.17 to v1.2.19
    
    * fix: unfollow jsx syntax

--- src/components/Layouts/PatchNews.tsx
+++ src/components/Layouts/PatchNews.tsx
@@ -7,11 +7,11 @@ export default function PatchNews() {
       className="bg-gradient-to-r text-white from-primary via-primary-light to-secondary h-8 py-1.5 lg:h-12 w-full flex flex-row text-center gap-x-1 items-center justify-center text-sm md:text-base font-semibold leading-normal"
     >
       <p className="hidden md:block">
-        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.17</span>{" "}
-        has been released with Next15
+        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.19</span>{" "}
+        has been released with <strong>Yarn</strong> supported
       </p>
       <p className="md:hidden">
-        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.17</span>{" "}
+        🚀 <strong>DevIniter</strong> <span className="text-gold">v1.2.19</span>{" "}
         is out now.
       </p>
       <Link
